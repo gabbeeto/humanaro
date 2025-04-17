f0a60763d8bd0c099ed09b6e89fb26753755cef8
gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2025-04-17 20:03:41 -0300

    I realized I need to employ another algorithm for the enemy

--- entities/enemy.gd
+++ entities/enemy.gd
@@ -13,17 +13,65 @@ var playerInArea: bool = false
 
 var enemyPath: Array[Dictionary]= [
 mkDirection(Direction.X, 2 ),
-mkDirection(Direction.Z, 4 , false)
+mkDirection(Direction.Z, 4 , false),
+mkDirection(Direction.X, 3 ),
+mkDirection(Direction.Z, 5 ),
 ]
 
 
 var currentMovementIndex: int = 0 
 
 var firstGlobalPosition: Vector3
+@export var pathToTravel: Array
 func entityStarts() -> void:
 	# because position changes
 	firstGlobalPosition = global_position
 	currentMovementIndex = 0
+
+	var currentGlobal :Vector3 = firstGlobalPosition
+	var pathLength: int = len(enemyPath)
+	for index in pathLength:
+		var currentPath: Dictionary = enemyPath[index]
+
+		var isXAxis: bool = currentPath['type'] == Direction.X
+		var isZAxis: bool = currentPath['type'] == Direction.Z
+
+
+		var isPositive: bool = currentPath['isPositive'] == true
+		var positiveSign: int = -1
+
+		if isPositive:
+			positiveSign = 1
+
+
+		var vectorSign: Vector2
+		if isXAxis:
+			currentGlobal.x +=  (currentPath['amount'] * positiveSign)
+			vectorSign.x = positiveSign
+			vectorSign.y = 0
+			print("isXAxis")
+		elif isZAxis:
+			currentGlobal.z +=  (currentPath['amount'] * positiveSign)
+			vectorSign.x = 0
+			vectorSign.y = positiveSign
+			print("isZAxis")
+		var pathArray: Array = [currentGlobal,vectorSign]
+		print(vectorSign)
+		print(pathArray)
+		pathToTravel.append(pathArray)
+
+	var index: int= pathLength -2
+	var copyOfPathToTravel: Array = pathToTravel.duplicate(true)
+	while index > -1:
+		var newPathToTravel: Array = copyOfPathToTravel[index]
+		newPathToTravel[1].x = -1 * copyOfPathToTravel[index + 1][1].x 
+		newPathToTravel[1].y = -1 * copyOfPathToTravel[index + 1][1].y 
+		pathToTravel.append(newPathToTravel)
+		index -= 1
+
+	print( pathToTravel )
+
+		# pathToTravel.append(currentPath)
 
 
 
@@ -31,67 +79,19 @@ func entityStarts() -> void:
 var EnemyDoneAllPath: bool = false
 
 func entityProcess(delta: float) -> void:
-
-	var currentPath: Dictionary = enemyPath[currentMovementIndex]
-	print(currentPath)
-
-	var directionIsPositive: bool = currentPath.isPositive
-	var isXAxis: bool = currentPath['type'] == Direction.X
-
-	var enemyReachedGoal: bool = false
-	
-	if isXAxis and directionIsPositive:
-		enemyReachedGoal =  global_position.x > (firstGlobalPosition.x + currentPath['amount']) 
-		if enemyReachedGoal:
-			velocity.x = 0
-			addCurrentMovement()
-			pass
-		else:
-			velocity.x += moveSpeed * delta
-	elif isXAxis and not directionIsPositive:
-		enemyReachedGoal =  global_position.x < (firstGlobalPosition.x - currentPath['amount'])
-		if enemyReachedGoal:
-			velocity.x = 0
-			addCurrentMovement()
-		else:
-			velocity.x -= moveSpeed * delta
-	elif not isXAxis and directionIsPositive:
-		enemyReachedGoal =  global_position.z > (firstGlobalPosition.z + currentPath['amount'])
-		if enemyReachedGoal:
-			velocity.z = 0
-			addCurrentMovement()
-		else:
-			velocity.z += moveSpeed * delta
-	elif not isXAxis and not directionIsPositive:
-		enemyReachedGoal =  global_position.z < (firstGlobalPosition.z - currentPath['amount'])
-		if enemyReachedGoal:
-			velocity.z = 0
-			addCurrentMovement()
-		else:
-			velocity.z -= moveSpeed * delta
+	pass
+	# make it so enemy traverse thought the pathToTravel and use the addCurrentMovement function to add movement
 
 
 
 
 
-var opositeDirectionAligntment: int = 1
+
 func addCurrentMovement()	-> void:
-
-	var nextPathIsLastPath: bool 
-
-	if EnemyDoneAllPath == false:
-		nextPathIsLastPath = currentMovementIndex + opositeDirectionAligntment >= len(enemyPath)
+	if currentMovementIndex + 1 >= len(pathToTravel):
+		currentMovementIndex = 0
 	else:
-
-		nextPathIsLastPath = currentMovementIndex + opositeDirectionAligntment < 0 
-
-	if nextPathIsLastPath  :
-		EnemyDoneAllPath = not EnemyDoneAllPath
-		opositeDirectionAligntment *= -1
-	
-
-
-	currentMovementIndex +=  opositeDirectionAligntment
+		currentMovementIndex +=  1
 
 
 
